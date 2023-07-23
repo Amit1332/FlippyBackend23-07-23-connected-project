2f38b4726e725a28761c7314cca744296b1e2461
Amitesh Singh <98410766+Amit1332@users.noreply.github.com>
2023-07-23 20:59:40 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const DatabaseConn = ()=>{
+    mongoose.set("strictQuery", false);
     mongoose.connect(process.env.DB_URI).then(()=>{
         console.log('database connection succesfull')
     }).catch(()=>{
